7b6c4112c3bb2b85182c111596e142bd1dbe06d7
soufiane <fahid.soufiane@gmail.com>
2018-01-15 13:40:30 +0000

    final version

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -14,7 +14,6 @@ class EventTest < ActiveSupport::TestCase
     assert_equal ["9:30", "10:00", "11:30", "12:00"], availabilities[1][:slots]
     assert_equal Date.new(2014, 8, 16), availabilities[6][:date]
     assert_equal 7, availabilities.length
-
   end
 
   test "multiple weekly recurring and not weekly recurring openings" do
@@ -32,7 +31,6 @@ class EventTest < ActiveSupport::TestCase
     assert_equal ["9:30", "10:00", "11:30", "13:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30"], availabilities[1][:slots]
     assert_equal Date.new(2014, 8, 15), availabilities[5][:date]
     assert_equal [], availabilities[0][:slots]
-
   end
 
   test "multiplte appointements without an opening" do
@@ -46,7 +44,6 @@ class EventTest < ActiveSupport::TestCase
     assert_equal Date.new(2014, 8, 8), availabilities[1][:date]
     assert_equal [], availabilities[0][:slots]
     assert_equal 7, availabilities.length
-
   end
 
   test "multiple opening events without appointements" do
@@ -61,7 +58,6 @@ class EventTest < ActiveSupport::TestCase
     assert_equal 17, availabilities[4][:slots].length
     assert_equal ["8:00", "8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00"], availabilities[4][:slots]
     assert_equal 7, availabilities.length
-
   end
 
   test "multiple opening events without appointements and weekly recurring openings" do
@@ -75,7 +71,6 @@ class EventTest < ActiveSupport::TestCase
     assert_equal DateTime.parse("2014-08-11"), availabilities[4][:date]
     assert_equal 8, availabilities[4][:slots].length
     assert_equal ["14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00"], availabilities[4][:slots]
-
   end
 
   test "example for future opening events creation" do
@@ -93,7 +88,6 @@ class EventTest < ActiveSupport::TestCase
     assert_equal Date.new(2014, 8, 3), availabilities[2][:date]
     assert_equal ["14:30", "16:00", "16:30", "17:00", "17:30", "18:00"], availabilities[2][:slots]
     assert_equal 7, availabilities.length
-
   end
 
   test "dupilcated opening with a booked day" do
@@ -111,7 +105,6 @@ class EventTest < ActiveSupport::TestCase
     assert_equal 1, availabilities[2][:slots].length
     assert_equal ["14:00"], availabilities[2][:slots]
     assert_equal 7, availabilities.length
-
   end
 
   test "separated openings" do
@@ -122,7 +115,6 @@ class EventTest < ActiveSupport::TestCase
 
     availabilities = Event.availabilities DateTime.parse("2014-08-05")
     assert_equal ["10:00", "10:30", "11:00", "11:30"], availabilities[0][:slots]
-
   end
 
   test "overlapping openings" do
@@ -134,7 +126,6 @@ class EventTest < ActiveSupport::TestCase
 
     availabilities = Event.availabilities DateTime.parse("2014-08-12")
     assert_equal 5, availabilities[0][:slots].length
-
   end
 
 
@@ -146,7 +137,6 @@ class EventTest < ActiveSupport::TestCase
 
     appointments = Event.appointments(DateTime.parse("2014-08-06"), DateTime.parse("2014-08-14"))
     assert_equal 2, appointments.length
-
   end
 
    test "day_opening_slots method" do
@@ -160,7 +150,6 @@ class EventTest < ActiveSupport::TestCase
     day_openings = Event.day_opening_slots(week_openings[the_day])
     assert_equal 2, day_openings.length
     assert_equal 7, week_openings.length
-
    end
 
   test "openings method" do
@@ -173,7 +162,6 @@ class EventTest < ActiveSupport::TestCase
 
     assert_equal 2, openings[the_day].length
     assert_equal 7, openings.length
-
   end
 
   test "available_slots_method" do
@@ -188,7 +176,6 @@ class EventTest < ActiveSupport::TestCase
     assert_equal DateTime.parse("2014-08-08 11:00"), available_slots[1]
     assert_equal DateTime.parse("2014-08-08 12:00"), available_slots[2]
     assert_equal 3, available_slots.length
-
   end
 
   test "available_slots_method with no appointment" do
@@ -203,7 +190,6 @@ class EventTest < ActiveSupport::TestCase
     assert_equal DateTime.parse("2014-08-08 10:00"), available_slots[1]
     assert_equal DateTime.parse("2014-08-08 10:30"), available_slots[2]
     assert_equal 6, available_slots.length
-
   end
 
   test "weekly_opening_to_fixed_opening method" do
@@ -213,7 +199,6 @@ class EventTest < ActiveSupport::TestCase
     fixed = Event.weekly_opening_to_fixed_opening(day, weekly)
     assert_equal DateTime.parse("2014-01-15 08:30"), fixed.starts_at
     assert_equal DateTime.parse("2014-01-15 10:00"), fixed.ends_at
-
   end
 
 end
